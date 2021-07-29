d7fc2158db0adf80f5c67973717ff0fc91ea63ce
Shaumik-Ashraf <a.shaumik@gmail.com>
2021-07-29 15:39:50 -0400

    tried implementing react routing

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Box, Link } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from '../Header/Header';
@@ -13,28 +14,48 @@ const App = props => (
   <ThemeProvider>
     <CssBaseline />
     <Header />
-    <HeroBar />
-    <Box
-      display="flex"
-      flexDirection="row"
-      justifyContent="center"
-    >
-      <Box mt={11} mr={4} maxWidth="530px">
-        <Form />
-      </Box>
-      <Box mt={2} ml={6} display="flex" flexDirection="column" alignItems="center">
-        <img src={illustration} alt="Woman receiving a vaccine" style={{maxWidth: '750px'}}/>
-        <Link
-          href="https://www.vecteezy.com/free-vector/epidemic-prevention"
-          color="secondary"
-          target="_blank"
-          rel="noopener"
-        >
-          Epidemic Prevention Vectors by Vecteezy
-        </Link>
-      </Box>
-    </Box>
 
+    <Router>    
+      <HeroBar />
+
+      <Switch>
+        <Route path="/">
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+          >
+            <Box mt={11} mr={4} maxWidth="530px">
+              <Form />
+            </Box>
+            <Box mt={2} ml={6} display="flex" flexDirection="column" alignItems="center">
+              <img src={illustration} alt="Woman receiving a vaccine" style={{maxWidth: '750px'}}/>
+              <Link
+                href="https://www.vecteezy.com/free-vector/epidemic-prevention"
+                color="secondary"
+                target="_blank"
+                rel="noopener"
+              >
+                Epidemic Prevention Vectors by Vecteezy
+              </Link>
+            </Box>
+          </Box>
+        </Route>
+
+        <Route path="/data-found">
+          <Link href="/">Back</Link>
+        
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+          >
+            SMART HEALTH CARD WILL GO HERE
+          </Box>
+        </Route>
+
+    </Switch>
+    </Router>
   </ThemeProvider>
 );
 
