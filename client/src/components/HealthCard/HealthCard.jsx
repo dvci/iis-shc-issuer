8ea4fe65c701ba625026047e8195d17fa5ac4656
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-08-02 18:53:40 -0400

    remove sample data

--- client/src/components/HealthCard/HealthCard.jsx
+++ client/src/components/HealthCard/HealthCard.jsx
@@ -57,7 +57,7 @@ function HealthCard(props) {
         Accept: "application/json",
         "Content-Type": "application/fhir+json",
       },
-      body: JSON.stringify(sampleData),
+      body: JSON.stringify(props.fhirBundle),
     })
       .then((response) => response.json())
       .then((responseJson) => {
@@ -214,89 +214,5 @@ function HealthCardVaccination(props) {
   );
 }
 
-const sampleData = {
-  resourceType: "Bundle",
-  type: "collection",
-  entry: [
-    {
-      fullUrl: "resource:0",
-      resource: {
-        resourceType: "Patient",
-        name: [
-          {
-            family: "ANYPERSON",
-            given: ["JOHN", "B."],
-          },
-        ],
-        birthDate: "1951-01-20",
-      },
-    },
-    {
-      fullUrl: "resource:1",
-      resource: {
-        resourceType: "Immunization",
-        meta: {
-          security: [
-            { system: "https://smarthealth.cards/ial", code: "IAL1.2" },
-          ],
-        },
-        status: "completed",
-        vaccineCode: {
-          coding: [
-            {
-              system: "http://hl7.org/fhir/sid/cvx",
-              code: "207",
-            },
-          ],
-        },
-        patient: {
-          reference: "resource:0",
-        },
-        occurrenceDateTime: "2021-01-01",
-        performer: [
-          {
-            actor: {
-              display: "ABC General Hospital",
-            },
-          },
-        ],
-        lotNumber: "0000001",
-      },
-    },
-    {
-      fullUrl: "resource:2",
-      resource: {
-        resourceType: "Immunization",
-        meta: {
-          security: [
-            { system: "https://smarthealth.cards/ial", code: "IAL1.2" },
-          ],
-        },
-        status: "completed",
-        vaccineCode: {
-          coding: [
-            {
-              system: "http://hl7.org/fhir/sid/cvx",
-              code: "207",
-            },
-          ],
-        },
-        patient: {
-          reference: "resource:0",
-        },
-        occurrenceDateTime: "2021-01-29",
-        performer: [
-          {
-            actor: {
-              display: "ABC General Hospital",
-            },
-          },
-        ],
-        lotNumber: "0000007",
-      },
-    },
-  ],
-};
-
-export { sampleData, HealthCard };
+export { HealthCard };
 export default HealthCard;
