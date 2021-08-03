73abbd12027cacb43bdd7229777d57d2669b3e95
mickohanlon23 <mohanlon@mitre.org>
2021-08-03 07:35:33 -0400

    Handle response HL7 Message from VXU Client

--- lib/iis_shc_issuer/vxu_client.rb
+++ lib/iis_shc_issuer/vxu_client.rb
@@ -8,24 +8,31 @@ module IISSHCIssuer
   module VXUClient
     module_function
 
-    # NOTE: This is a helper method to upload a patient to the IIS Sandbox and should be used for testing purposes only
     # Send a VXU message to the IIS Sandbox service to upload a patient
     # @param vxu_path [String] File Path where HL7 V2 VXU message is located
-    def upload_patient(vxu_path = 'lib/assets/vxu_fixtures/vxu.hl7')
+    def upload_patient(vxu_path)
       # Define client
       service_def = 'lib/assets/service.wsdl'
       client = Savon.client(wsdl: service_def,
                             endpoint: "#{Rails.application.config.iisSandboxHost}/iis-sandbox/soap",
                             pretty_print_xml: true)
       # Upload Patient from fixture
-      upload_raw_input = FILE.open(vxu_path).readlines
+      upload_raw_input = File.open(vxu_path).readlines
       upload_msg_input = HL7::Message.new(upload_raw_input)
-      client.call(:submit_single_message) do
-        message({ username: Rails.application.config.username,
-                  password: Rails.application.config.password,
-                  facilityID: Rails.application.config.facilityID,
-                  hl7Message: upload_msg_input })
+      begin
+        response = client.call(:submit_single_message) do
+          message({ username: Rails.application.config.username,
+                    password: Rails.application.config.password,
+                    facilityID: Rails.application.config.facilityID,
+                    hl7Message: upload_msg_input })
+        end
+      rescue Savon::Error => e
+        fault_code = e.to_s
+        raise IISSHCIssuer::SOAPError, fault_code
       end
+      raw_response_message = response.body[:submit_single_message_response][:return]
+      response_segments = raw_response_message.to_s.split("\n")
+      HL7::Message.new(response_segments)
     end
   end
 end
--- test/iis_shc_issuer/vxu_client_test.rb
+++ test/iis_shc_issuer/vxu_client_test.rb
@@ -15,7 +15,7 @@ class VXUClientTest < ActiveSupport::TestCase
   test 'Patient can be uploaded to IIS Sandbox via VXU message' do
     VCR.use_cassette('patient_is_uploaded') do
       # Enter VXU Upload fixture path parameter here
-      v2_response_body = IISSHCIssuer::VXUClient.upload_patient()
+      v2_response_body = IISSHCIssuer::VXUClient.upload_patient('test/fixtures/files/VXU_example.hl7')
       assert_instance_of(HL7::Message, v2_response_body)
     end
   end
