c9c60186c0917fc85ffddf17e6fe3cbd929e0843
mickohanlon23 <mohanlon@mitre.org>
2021-08-04 08:25:59 -0400

    Remove unused error class and require newly added iis_shc_issuer/ files within lib/iis_shc_issuer.rb

--- lib/iis_shc_issuer.rb
+++ lib/iis_shc_issuer.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'iis_shc_issuer/qbp_client'
 require 'iis_shc_issuer/errors'
-
-module IISSHCIssuer
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require 'iis_shc_issuer/profile_errors'
+require 'iis_shc_issuer/qbp_client'
+require 'iis_shc_issuer/qpd'
+require 'iis_shc_issuer/v2_message_builder'
+require 'iis_shc_issuer/v2_to_fhir'
+require 'iis_shc_issuer/vxu_client'
