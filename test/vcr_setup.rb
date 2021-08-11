e2b2f25346a9750dd1ad4851a3a8486e18685c23
mickohanlon23 <mohanlon@mitre.org>
2021-08-11 13:11:32 -0400

    Configure VCR test mockings to ignore hostnames

--- test/vcr_setup.rb
+++ test/vcr_setup.rb
@@ -3,4 +3,5 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'test/vcr_cassettes/iis_shc_issuer'
   config.hook_into :webmock
+  config.default_cassette_options = { match_requests_on: [:method, :path] }
 end
