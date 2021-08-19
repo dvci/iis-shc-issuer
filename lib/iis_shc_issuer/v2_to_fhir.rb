341b393435fc1f26b4fb28422f1291f4804e51c2
mickohanlon23 <mohanlon@mitre.org>
2021-08-19 15:29:39 -0400

    Move v2 converter API version to host config rather than hardcoded within library

--- lib/iis_shc_issuer/v2_to_fhir.rb
+++ lib/iis_shc_issuer/v2_to_fhir.rb
@@ -9,7 +9,7 @@ module IISSHCIssuer
     # @param v2_response [HL7::Message] V2 message returned from the IIS Sandbox
     # @return [String] FHIR Bundle representation of the V2 message
     def translate_to_fhir(v2_response)
-      fhir_response = Faraday.post("#{Rails.application.config.v2_to_fhir_host}/api/v0.1.0/convert/text",
+      fhir_response = Faraday.post("#{Rails.application.config.v2_to_fhir_host}/convert/text",
                                    v2_response.to_hl7,
                                    'Content-Type' => 'text/plain')
       fhir_response.body
