c52bb1010503ac4fb645767c61d48c0a3c51e119
mickohanlon23 <mohanlon@mitre.org>
2021-08-19 16:05:34 -0400

    Overwrite cassette config for converter tests so that api version change does not cause an error

--- test/iis_shc_issuer/v2_to_fhir_test.rb
+++ test/iis_shc_issuer/v2_to_fhir_test.rb
@@ -1,18 +1,13 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'vcr_setup'
 require 'iis_shc_issuer/v2_to_fhir'
-require 'vcr'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'test/vcr_cassettes/iis_shc_issuer'
-  config.hook_into :webmock
-end
 
 class V2ToFHIRTest < ActiveSupport::TestCase
   # General Functionality Tests
   test 'translate_to_fhir() method successfully returns a stringified JSON object' do
-    VCR.use_cassette('translator_returns_json_string') do
+    VCR.use_cassette('translator_returns_json_string', :match_requests_on => [:method]) do
       response = File.open('test/fixtures/files/RSP_valid.hl7').readlines
       v2_response_body = HL7::Message.new(response)
       fhir_response_body = IISSHCIssuer::V2ToFHIR.translate_to_fhir(v2_response_body)
@@ -28,7 +23,7 @@ class V2ToFHIRTest < ActiveSupport::TestCase
   # V2 to FHIR Translation Tests
 
   test 'Valid HL7 V2 Complete Immunization History Response will return a FHIR Bundle from the HL7 to V2 Translator' do
-    VCR.use_cassette('translator_returns_fhir_bundle') do
+    VCR.use_cassette('translator_returns_fhir_bundle', :match_requests_on => [:method]) do
       response = File.open('test/fixtures/files/RSP_valid.hl7').readlines
       v2_response = HL7::Message.new(response)
       fhir_response = IISSHCIssuer::V2ToFHIR.translate_to_fhir(v2_response)
@@ -38,7 +33,7 @@ class V2ToFHIRTest < ActiveSupport::TestCase
   end
 
   test 'Non-Patient HL7 V2 Response will return an error message from the HL7 to V2 Translator' do
-    VCR.use_cassette('translator_returns_error') do
+    VCR.use_cassette('translator_returns_error', :match_requests_on => [:method]) do
       response = File.open('test/fixtures/files/RSP_error.hl7').readlines
       v2_response = HL7::Message.new(response)
       fhir_response = IISSHCIssuer::V2ToFHIR.translate_to_fhir(v2_response)
