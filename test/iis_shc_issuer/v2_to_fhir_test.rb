bc345f732610085aab2acfac13845f993d69db35
mickohanlon23 <mohanlon@mitre.org>
2021-08-19 16:07:38 -0400

    Bump v2 converter api endpoint to 0.2.0

--- test/iis_shc_issuer/v2_to_fhir_test.rb
+++ test/iis_shc_issuer/v2_to_fhir_test.rb
@@ -7,7 +7,7 @@ require 'iis_shc_issuer/v2_to_fhir'
 class V2ToFHIRTest < ActiveSupport::TestCase
   # General Functionality Tests
   test 'translate_to_fhir() method successfully returns a stringified JSON object' do
-    VCR.use_cassette('translator_returns_json_string', :match_requests_on => [:method]) do
+    VCR.use_cassette('translator_returns_json_string', match_requests_on: [:method]) do
       response = File.open('test/fixtures/files/RSP_valid.hl7').readlines
       v2_response_body = HL7::Message.new(response)
       fhir_response_body = IISSHCIssuer::V2ToFHIR.translate_to_fhir(v2_response_body)
@@ -23,7 +23,7 @@ class V2ToFHIRTest < ActiveSupport::TestCase
   # V2 to FHIR Translation Tests
 
   test 'Valid HL7 V2 Complete Immunization History Response will return a FHIR Bundle from the HL7 to V2 Translator' do
-    VCR.use_cassette('translator_returns_fhir_bundle', :match_requests_on => [:method]) do
+    VCR.use_cassette('translator_returns_fhir_bundle', match_requests_on: [:method]) do
       response = File.open('test/fixtures/files/RSP_valid.hl7').readlines
       v2_response = HL7::Message.new(response)
       fhir_response = IISSHCIssuer::V2ToFHIR.translate_to_fhir(v2_response)
@@ -33,7 +33,7 @@ class V2ToFHIRTest < ActiveSupport::TestCase
   end
 
   test 'Non-Patient HL7 V2 Response will return an error message from the HL7 to V2 Translator' do
-    VCR.use_cassette('translator_returns_error', :match_requests_on => [:method]) do
+    VCR.use_cassette('translator_returns_error', match_requests_on: [:method]) do
       response = File.open('test/fixtures/files/RSP_error.hl7').readlines
       v2_response = HL7::Message.new(response)
       fhir_response = IISSHCIssuer::V2ToFHIR.translate_to_fhir(v2_response)
