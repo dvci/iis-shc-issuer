9a8b7648186820740fbd8af4990252cea9728ac3
Mick O'Hanlon <mohanlon@mitre.org>
2022-03-27 14:34:03 -0400

    Remove non-existant fields from additional information patient controller test

--- test/controllers/patient_controller_test.rb
+++ test/controllers/patient_controller_test.rb
@@ -51,9 +51,9 @@ class PatientControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'search for patient with additional information' do
-    params = { given_name: 'VallisAIRA', family_name: 'RogersAIRA', patient_dob: '20160715', street: '28 Wally Lane Rd',
-               city: 'Boston', state: 'MA', zip: '00012', mothers_maiden_name: 'Jane', name_code: 'M',
-               phone: '1234567890', sex: 'M' }
+    params = { given_name: 'VallisAIRA', family_name: 'RogersAIRA', patient_dob: '20160715', street: '1371 Kongeradeel Ave',
+               city: 'Quincy', state: 'MI', zip: '49082', mothers_maiden_name: 'BellAIRA', name_code: 'M',
+               phone: '5175138248', sex: 'M' }
 
     VCR.use_cassette('patient_search') do
       get search_path, params: params
@@ -66,25 +66,14 @@ class PatientControllerTest < ActionDispatch::IntegrationTest
     resources = bundle.entry.map(&:resource)
     assert_equal 3, resources.length
 
-    # puts (bundle.to_json)
     patient = resources[0]
-    # puts (patient.to_json)
 
     assert patient.is_a?(FHIR::Patient)
     name = patient.name.first
     address = patient.address
-    mothers_maiden_nm = patient.mothers_maiden_name
     assert_equal params[:given_name], name.given&.first
     assert_equal params[:family_name], name.family
     assert_equal params[:patient_dob], patient.birthDate
-    assert_equal params[:street], address.street
-    assert_equal params[:city], address.city
-    assert_equal params[:state], address.state
-    assert_equal params[:zip], address.zip
-    assert_equal params[:mothers_maiden_name], mothers_maiden_nm.mothers_maiden_name
-    assert_equal params[:name_code], mothers_maiden_nm.name_code
-    assert_equal params[:phone], patient.phone
-    assert_equal params[:sex], patient.admin_sex
 
     resources[1, 2].each { |imm| assert imm.is_a?(FHIR::Immunization) }
   end
